f3ca93488b52cbaae011891d664c0bbb9df0626f
Souhailbnl <souhailbnl@hotmail.com>
2024-11-14 14:36:29 +0100

    E1_Loop

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         for (int i = 36; i <= 60; i++) {
             if (i % 2 == 0){
-                System.out.println("Alle getallen even tussen 36 en 60: " + i);
+                System.out.println(i);
             }
 
 
